0beead37c918e1bee523486964a55906f39f7f75
mkf <mkf@KF>
2024-09-03 17:26:33 +0300

    Add project files.

---
+++ Sorts/Node.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Sorts
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+        public Node GetValue()
+        {
+            return data;
+        }
+        public void SetValue(int data)
+        {
+            this.data = data;
+        }
+        public Node GetValue()
+        {
+            return next;
+        }
+        public void SetValue(Node next)
+        {
+            this.next = next;
+        }
+    }
+    public class LinkedList
+    {
+        public Node Head;
+        public LinkedList() { }
+        public LinkedList(Node head)
+        {
+            Head = head;
+        }
+
+        public void Add(int data)
+        {
+            Node newNode = new Node(data);
+                Node lastNode = Head;
+                while(lastNode.next != null)
+                {
+                    lastNode = lastNode.next;
+                }
+                lastNode.next = newNode;
+        }
+
+        public string Display()
+        {
+            string str = "";
+            Node node = Head;
+            while (node.next != null)
+            {
+                str += node.data.ToString();
+                node = node.next;
+            }
+            str += node.data.ToString();
+            return str;
+        }
+
+        public int Length()
+        {
+            int counter = 0;
+            Node node = Head;
+            while (node.next != null)
+            {
+                counter++;
+                node = node.next;
+            }
+            counter++;
+            return counter;
+        }
+
+        public void RemoveValue(int data)
+        {
+            Node node = Head;
+            Node prev = Head;
+            while (node.next != null)
+            {
+                if(node.data == data)
+                {
+                    prev.next = node;
+                    break;
+                }
+                node = node.next;
+                prev = node;
+            }
+        }
+
+        public void RemoveAllValues(int data)
+        {
+            Node node = Head;
+            Node prev = Head;
+            while (node.next != null)
+            {
+                if (node.data == data)
+                {
+                    prev.next = node;
+                }
+                node = node.next;
+                prev = node;
+            }
+        }
+
+        public void RemoveIndex(int data)
+        {
+
+        }
+    }
+}
---
+++ Sorts/SortingAlgtms.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorts
+{
+    public class SortingAlgtms
+    {
+        // TODO selection sort
+        // TODO insertion sort
+        // TODO merge sort
+        // TODO finish bubble sort
+        static int[] BubbleSort(int[] arr, int num)
+        {
+            int temp = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+
+                    }
+                }
+                // Unfinished
+            }
+        }
+}
